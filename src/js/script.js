f8c4b765dbe62dfe527cff9e742eb13975a0a4e5
barrcarballo <barbie.carb@gmail.com>
2024-11-07 10:23:45 -0300

    deleting profile and capacitor

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
-import { Preferences } from '@capacitor/preferences';
-
 function hideAllSections(){
-   Array.from(document.querySelectorAll(".game")).concat([document.getElementById("main"), document.getElementById("profile")])
+   Array.from(document.querySelectorAll(".game")).concat([document.getElementById("main")])
     .forEach (element => element.classList.add("nodisp"))
 }
 function showSections(sectionId){
@@ -22,42 +20,9 @@ function setupButtons(){
     });
 }
 
-async function getPreference(key){
-    const { value } = await Preferences.get({ key });
-    return value === null ? null : JSON.parse(value);
-}
-
-async function setPreferences(key, obj) {
-    await Preferences.set({key, value: JSON.stringify(obj)});
-}
-
 function initApp(){
     setupButtons();
 
-    getPreference("config").then(config =>{
-        hideAllSections();
-        if(config === null){
-            showSections("profile");
-        } else {
-            showSections("main");
-        }
-    })
 }
-
-document.getElementById("savePreferences").addEventListener("click", e => {
-    e.preventDefault();
-    const config = {
-        name : document.getElementById("name").value, 
-        nickname : document.getElementById("nickname").value
-    };
-    if(config.name.length === 0 || config.nickname.length === 0){
-        alert("You must complete the fields")
-    }else{
-        setPreferences("config", config).then(()=> {
-            hideAllSections();
-            showSections("main");
-        })
-    }
-})
 
 document.addEventListener("DOMContentLoaded", initApp);
