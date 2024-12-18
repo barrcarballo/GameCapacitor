b5a9b19d058ebf94dee42457142e79c3376d30d7
barrcarballo <barbie.carb@gmail.com>
2024-12-18 15:56:48 -0300

    simon says finished y cambios de px a ems

---
+++ src/js/scriptSimon.js
@@ -0,0 +1,97 @@
+const colors = ["green", "red", "yellow", "blue"];
+let sequence = [];
+let playerSequence = [];
+let level = 0;
+
+// elementos del DOM
+const startButton = document.getElementById("start-button");
+const gameBoard = document.getElementById("game-board");
+const colorElements = colors.map(color => document.getElementById(color));
+const modal = document.getElementById("game-over-modal");
+const closeModalButton = document.getElementById("close-modal");
+const finalLevelText = document.querySelector(".final-level");
+
+
+function startGame() {
+  sequence = [];
+  playerSequence = [];
+  level = 0;
+  startButton.disabled = true;
+  closeModal();
+  nextRound();
+}
+
+function nextRound() {
+  level++;
+  document.querySelectorAll(".final-level").forEach(el => el.textContent = level); // escribe el nivel
+  playerSequence = [];
+  const nextColor = colors[Math.floor(Math.random() * colors.length)];
+  sequence.push(nextColor);
+  displaySequence();
+}
+
+function displaySequence() {
+  let delay = 500;
+  sequence.forEach((color, index) => {
+    setTimeout(() => activateColor(color), delay * (index + 1));
+  });
+  
+  setTimeout(() => enablePlayerInput(), delay * sequence.length + 500);
+}
+
+function playSound(color) {
+  const audio = new Audio(`./assets/sounds/${color}.mp3`);
+  audio.play();
+}
+
+function activateColor(color) {
+  const element = document.getElementById(color);
+  element.classList.add("active");
+  playSound(color); // sonido
+  setTimeout(() => element.classList.remove("active"), 300);
+}
+
+function enablePlayerInput() {
+  gameBoard.addEventListener("click", handlePlayerInput);
+}
+
+function disablePlayerInput() {
+  gameBoard.removeEventListener("click", handlePlayerInput);
+}
+
+function handlePlayerInput(event) {
+  const { id } = event.target;
+  if (!colors.includes(id)) return;
+  
+  playerSequence.push(id);
+  activateColor(id); //reproduce el sonido
+  
+  const currentStep = playerSequence.length - 1;
+  if (playerSequence[currentStep] !== sequence[currentStep]) {
+    gameOver();
+    return;
+  }
+  
+  if (playerSequence.length === sequence.length) {
+    disablePlayerInput();
+    setTimeout(nextRound, 1000);
+  }
+}
+
+function gameOver() {
+  finalLevelText.textContent = level;
+  modal.style.display = "flex";
+  startButton.disabled = false;
+  disablePlayerInput();
+}
+
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Comienza el juego
+startButton.addEventListener("click", startGame);
+closeModalButton.addEventListener("click", closeModal);
+
+
